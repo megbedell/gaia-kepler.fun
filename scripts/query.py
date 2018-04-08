f9f678480b5079b3126bea5da20b4b06e28b90ce
megbedell <megbedell@gmail.com>
2018-04-07 22:20:53 -0400

    k2 downloads and plotting script

--- scripts/query.py
+++ scripts/query.py
@@ -16,7 +16,10 @@ EXOPLANETS_CSV_URL = ('http://exoplanetarchive.ipac.caltech.edu/cgi-bin/'
                       'nstedAPI/nph-nstedAPI?table=exoplanets')
 KOIS_CSV_URL = ('http://exoplanetarchive.ipac.caltech.edu/cgi-bin/'
                       'nstedAPI/nph-nstedAPI?table=q1_q17_dr25_koi')
-
+K2TARGETS_CSV_URL = ('http://exoplanetarchive.ipac.caltech.edu/cgi-bin/'
+                      'nstedAPI/nph-nstedAPI?table=k2targets')
+K2CAND_CSV_URL = ('http://exoplanetarchive.ipac.caltech.edu/cgi-bin/'
+                      'nstedAPI/nph-nstedAPI?table=k2candidates')
                       
 def get_table(url=None, cache=True, show_progress=True,
                     table_path=None, select=None):
@@ -92,7 +95,6 @@ def get_kois_table(cache=True, show_progress=True,
     description of the columns can be found `here
     <https://exoplanetarchive.ipac.caltech.edu/docs/API_kepcandidate_columns.html>`_
     """
-    select = None
     koi_table = get_table(url=KOIS_CSV_URL, cache=cache, 
                                         show_progress=show_progress, 
                                         table_path=table_path, select=select)
@@ -109,12 +111,43 @@ def get_keplerstellar_table(cache=True, show_progress=True,
     description of the columns can be found `here
     <https://exoplanetarchive.ipac.caltech.edu/docs/API_keplerstellar_columns.html>`_
     """
-    select = None
     keplerstellar_table = get_table(url=STELLAR_CSV_URL, cache=cache, 
                                         show_progress=show_progress, 
                                         table_path=table_path, select=select)
     
     return keplerstellar_table
+    
+def get_k2targets_table(cache=True, show_progress=True,
+                                table_path=None, select=None):
+    """
+    Download (and optionally cache) the `NExScI Exoplanet Archive K2
+    Targets table <http://exoplanetarchive.ipac.caltech.edu/index.html>`_.
+
+    The Exoplanet Archive table returns lots of columns of data. A full
+    description of the columns can be found `here
+    <https://exoplanetarchive.ipac.caltech.edu/docs/API_k2_columns.html>`_
+    """
+    k2targets_table = get_table(url=K2TARGETS_CSV_URL, cache=cache, 
+                                        show_progress=show_progress, 
+                                        table_path=table_path, select=select)
+    
+    return k2targets_table
+    
+def get_k2candidates_table(cache=True, show_progress=True,
+                                table_path=None, select=None):
+    """
+    Download (and optionally cache) the `NExScI Exoplanet Archive K2
+    Candidates table <http://exoplanetarchive.ipac.caltech.edu/index.html>`_.
+
+    The Exoplanet Archive table returns lots of columns of data. A full
+    description of the columns can be found `here
+    <https://exoplanetarchive.ipac.caltech.edu/docs/API_k2candidates_columns.html>`_
+    """
+    k2candidates_table = get_table(url=K2CAND_CSV_URL, cache=cache, 
+                                        show_progress=show_progress, 
+                                        table_path=table_path, select=select)
+    
+    return k2candidates_table
     
 def get_alias_table(cache=True, show_progress=True,
                                 table_path=None, select=None):
@@ -150,9 +183,9 @@ def save_output(table, filename):
     
 if __name__ == "__main__":
     dist = 1 # arcsec radius of query
-    cat = 'src' # 'src' or 'tgas'
+    cat = 'tgas' # 'src' or 'tgas'
     remake_csvs = False
-    make_plots = True
+    make_plots = False
     
     # OG Kepler long-cadence targets:
     lc_file = 'kic_lc_coords.csv'  
@@ -199,6 +232,57 @@ if __name__ == "__main__":
     # TODO: flag confirmed
     
     save_output(lc_table, '../data/lc_{c}_{d}arcsec'.format(d=dist, c=cat))    
+    
+    # K2 targets:
+    k2_file = 'epic_coords.csv'  
+    #select = 'epic_number,tm_name,k2_campaign_str,k2_type,ra,dec,k2_lcflag,k2_scflag'
+    #select += ',k2_teff,k2_tefferr1,k2_tefferr2,k2_logg,k2_loggerr1,k2_loggerr2'
+    #select += ',k2_metfe,k2_metfeerr1,k2_metfeerr2,k2_rad,k2_raderr1,k2_raderr2'
+    #select += ',k2_mass,k2_masserr1,k2_masserr2'
+    select=None
+    k2_nasa_table = get_k2targets_table(select=select)
+    epic = k2_nasa_table['epic_number']
+     
+    if remake_csvs: # make the file
+        print('making K2 target file...')
+        k2_coords_table = get_k2targets_table(select='epic_number,ra,dec')
+        ra, dec = k2_coords_table['ra'], k2_coords_table['dec']
+        f = open(k2_file, 'w')
+        f.write('epic_number, RA, Dec\n')
+        for e,(r,d) in tqdm(zip(epic, zip(ra, dec))):
+            f.write('{0}, {1:.8f}, {2:.8f}\n'.format(e, r, d))
+        f.close()
+        print('file made')
+    
+
+    k2_xmatch_table = xmatch_cds_from_csv(k2_file, ra_name='RA', dec_name='Dec', dist=dist, cat=cat)
+    k2_table = join(k2_xmatch_table, k2_nasa_table, keys='epic_number', table_names=['gaia', 'nasa'], 
+                    join_type='left')
+    
+    if make_plots:  # plots 
+        print('making K2 plots...')
+        k2_matches = [np.sum(k2_table['epic_number'] == e) for e in tqdm(epic)]
+        plt.hist(k2_matches)
+        plt.ylabel('Sources')
+        plt.xlabel('Matches')
+        plt.savefig('k2_{c}_matches_{d}arcsec.png'.format(d=dist, c=cat))
+        plt.clf()
+    
+        plt.hist(k2_table['angDist'], bins=np.arange(0.,dist,0.1))
+        plt.ylabel('Sources')
+        plt.xlabel('Angular Dist (arcsec)')
+        plt.savefig('k2_{c}_angdist_{d}arcsec.png'.format(d=dist, c=cat))
+        plt.clf()
+        
+    # flag KOIs and confirmed hosts:
+    full_k2cand_table = get_k2candidates_table()
+    k2cand_table = unique(full_k2cand_table, keys='epic_name')
+    k2cand_table_to_join = k2cand_table['k2c_disp','k2c_note']
+    epic_numbers = [int(n.split(" ")[1]) for n in k2cand_table['epic_name']]
+    k2cand_table_to_join['epic_number'] = epic_numbers
+    k2_table = join(k2_table, k2cand_table_to_join, keys='epic_number', join_type='left')
+    
+    save_output(k2_table, '../data/k2_{c}_{d}arcsec'.format(d=dist, c=cat))    
     
     
     # confirmed planets:
