121c285ffb612d2160f198bbad81737724cc2194
megbedell <megbedell@gmail.com>
2022-06-15 15:28:11 -0400

    DR3 update

--- scripts/build_tables_w_dist.py
+++ scripts/build_tables_w_dist.py
@@ -1,52 +1,27 @@
-from astropy.table import QTable, Table, Column, MaskedColumn, join, unique, vstack
+from astropy.table import Table, join, unique
 import numpy as np
-import matplotlib.pyplot as plt
 from astropy import units as u
-from astropy.io import fits, ascii
+from astropy.io import fits
 import astropy.coordinates as coord
 from astropy.time import Time
 from units import gaia_unit_map, kepler_unit_map
 from nasa_tables import *
 from pyia import GaiaData 
-import pdb
 
 no_dist = True # flag to not include Bailer-Jones distances
 
-def clean_gaia_table(tbl, kepler=False, k2=False,exoplanets=False):
+def clean_gaia_table(tbl, kepler=False, k2=False,exoplanets=False, ang_dist_key='kepler_gaia_ang_dist'):
     """
     Add units, delete some columns
     """
-    '''
-    cols_to_delete = ['solution_id', 'random_index', 'astrometric_n_obs_al', 'astrometric_n_obs_ac', 
-                      'astrometric_n_good_obs_al', 'astrometric_n_bad_obs_al', 'astrometric_gof_al',
-                      'astrometric_params_solved', 'astrometric_weight_al', 'astrometric_pseudo_colour',
-                      'astrometric_pseudo_colour_error', 'mean_varpi_factor_al', 'astrometric_matched_observations',
-                      'visibility_periods_used', 'astrometric_sigma5d_max',  
-                      'frame_rotator_object_type', 'matched_observations', 'phot_g_n_obs', 
-                      'phot_g_mean_flux_over_error', 'phot_bp_n_obs', 'phot_bp_mean_flux_over_error',
-                      'phot_rp_n_obs', 'phot_rp_mean_flux_over_error', 
-                      'phot_proc_mode', 'rv_nb_transits', 'rv_template_teff', 'rv_template_logg', 
-                      'rv_template_fe_h', 'priam_flags', 'flame_flags', 'datalink_url', 'epoch_photometry_url',
-                      'phot_bp_n_contaminated_transits', 'phot_bp_n_blended_transits',
-                      'phot_rp_n_contaminated_transits', 'phot_rp_n_blended_transits',
-                      'scan_direction_strength_k1', 'scan_direction_mean_k1',
-                      'scan_direction_strength_k2', 'scan_direction_mean_k2',
-                      'scan_direction_strength_k3', 'scan_direction_mean_k3',
-                      'scan_direction_strength_k4', 'scan_direction_mean_k4',
-                      'ipd_gof_harmonic_amplitude', 'ipd_gof_harmonic_phase', 
-                      'ipd_frac_multi_peak', 'ipd_frac_odd_win'
-                    ]
-    tbl.remove_columns(cols_to_delete)
-    '''
     if kepler:
-        #cols_to_delete = ['kepler_oid', 'angdist']
         cols_to_delete = ['kepler_oid']
         tbl.remove_columns(cols_to_delete)
     if k2:
-        cols_to_delete = ['k2_oid', 'angdist']
+        cols_to_delete = ['k2_oid']
         tbl.remove_columns(cols_to_delete)
     if exoplanets:
-        cols_to_delete = ['exoplanets_oid', 'angdist']
+        cols_to_delete = ['exoplanets_oid']
         tbl.remove_columns(cols_to_delete)
         for i,p in enumerate(tbl['pl_name']):
             tbl['pl_name'][i] = p.strip() # remove whitespace
@@ -54,7 +29,10 @@ def clean_gaia_table(tbl, kepler=False, k2=False,exoplanets=False):
         if col in gaia_unit_map:
             if not isinstance(gaia_unit_map[col], u.UnrecognizedUnit): # unit is valid
                 tbl[col].unit = gaia_unit_map[col]    
-    tbl.rename_column('ref_epoch', 'gaia_ref_epoch')     
+    tbl.rename_column('ref_epoch', 'gaia_ref_epoch')   
+    tbl.rename_column('angdist', ang_dist_key)  
+    tbl[ang_dist_key] = tbl[ang_dist_key].to(u.arcsec)
+    tbl['pm_corrected'] = False
     return tbl
     
 def clean_dist_table(tbl):
@@ -87,17 +65,18 @@ def make_full_tables(data_dir='../data/',kepler=False,k2=False,exoplanets=False)
     if kepler:
         gaia_matches_file = data_dir+'kepler_30arcsec_gaia.fits'
         dist_table_file = data_dir+'kepler_30arcsec_dist.fits'
-        outfile_20arcsec = 'kepler_dr2_20arcsec.fits'
-        outfile_4arcsec = 'kepler_dr2_4arcsec.fits'
-        outfile_1arcsec = 'kepler_dr2_1arcsec.fits'
+        outfile_prefix = 'kepler_dr3_'
         
+        '''
         select = 'kepid,tm_designation,kepmag'
         select += ',teff,teff_err1,teff_err2,teff_prov,logg,logg_err1,logg_err2,logg_prov'
         select += ',feh,feh_err1,feh_err2,feh_prov,radius,radius_err1,radius_err2'
         select += ',mass,mass_err1,mass_err2,prov_sec,nconfp,nkoi,ntce,jmag,hmag,kmag'
         nasa_table = get_keplerstellar_table(select=select, cache=False)
+        '''
+        nasa_table = Table.read('/Users/mbedell/Documents/Web Design/gaia-kepler.fun/data/kepler_table.csv') # since SSL is failing
         nasa_table = clean_kepler_table(nasa_table)
-        
+    
         # make custom planet host status column
         nasa_table['planet?'] = 'none'
         nasa_table['planet?'][nasa_table['nkoi'] > 0] = 'cand'
@@ -110,9 +89,8 @@ def make_full_tables(data_dir='../data/',kepler=False,k2=False,exoplanets=False)
     elif k2:
         gaia_matches_file = data_dir+'k2_30arcsec_gaia.fits'
         dist_table_file = data_dir+'k2_30arcsec_dist.fits'
-        outfile_20arcsec = 'k2_dr2_20arcsec.fits'
-        outfile_4arcsec = 'k2_dr2_4arcsec.fits'
-        outfile_1arcsec = 'k2_dr2_1arcsec.fits'
+        outfile_prefix = 'k2_dr3_'
+
         
         select = 'epic_number,tm_name,k2_campaign_str,k2_type,k2_lcflag,k2_scflag'
         select += ',k2_teff,k2_tefferr1,k2_tefferr2,k2_logg,k2_loggerr1,k2_loggerr2'
@@ -136,7 +114,7 @@ def make_full_tables(data_dir='../data/',kepler=False,k2=False,exoplanets=False)
     elif exoplanets:
         gaia_matches_file = data_dir+'exoplanets_10arcsec_gaia.fits'
         dist_table_file = data_dir+'exoplanets_10arcsec_dist.fits'
-        outfile_1arcsec = 'exoplanets_dr2_1arcsec.fits'
+        outfile_prefix = 'exoplanets_dr3_'
         
         select = 'pl_hostname,pl_letter,pl_name,pl_discmethod,pl_controvflag,pl_pnum,pl_orbper'
         select += ',pl_orbpererr1,pl_orbpererr2,pl_orbperlim,pl_orbpern,pl_orbsmax,pl_orbsmaxerr1,pl_orbsmaxerr2'
@@ -167,7 +145,8 @@ def make_full_tables(data_dir='../data/',kepler=False,k2=False,exoplanets=False)
     
     hdus = fits.open(gaia_matches_file)
     gaia_matches_tbl = Table(hdus[1].data)
-    gaia_matches_tbl = clean_gaia_table(gaia_matches_tbl, kepler=kepler, k2=k2, exoplanets=exoplanets)
+    gaia_matches_tbl = clean_gaia_table(gaia_matches_tbl, kepler=kepler, k2=k2, 
+                                        exoplanets=exoplanets, ang_dist_key=ang_dist_key)
     if no_dist:
         table = join(gaia_matches_tbl, nasa_table, keys=nasa_table_key)
     else:
@@ -181,37 +160,40 @@ def make_full_tables(data_dir='../data/',kepler=False,k2=False,exoplanets=False)
     table.sort('source_id')
     coordtable = GaiaData(gaia_matches_file)
     coordtable.data.sort('source_id')
+    #coordtable.data['radial_velocity'][~np.isfinite(coordtable.data)] = 0.0 * u.km/u.s # mask nans
     gaiaCoord = coordtable.get_skycoord(distance=coordtable.get_distance(min_parallax=1e-3*u.mas,
-                                    parallax_fill_value=1e-5*u.mas))
-    # NOTE: the above does not use RVs at all. will need to deal with EDR3 naming issue.
-       
-    refCoord = coord.SkyCoord(ra=table[ra_key], dec=table[dec_key], 
-                            obstime='J2000') # KIC/EPIC/NExScI coord epoch
-    ref_time = refCoord.obstime
+                                    parallax_fill_value=1e-5*u.mas),
+                                   radial_velocity=coordtable.get_radial_velocity(fill_value=0.*u.km/u.s))
+    
+    pm_threshold = 0.05 #* u.arcsec/u.yr -- at/above this, apply PM corrections
+    fast_mask = np.sqrt(table['pmra'].to(u.arcsec/u.yr).value**2 
+                        + table['pmdec'].to(u.arcsec/u.yr).value**2) >= pm_threshold
+    gaiaCoord_fast = gaiaCoord[fast_mask]
+    refCoord_fast = coord.SkyCoord(ra=table[ra_key][fast_mask], dec=table[dec_key][fast_mask], 
+                            obstime='J2000') # KIC/EPIC/NExScI coord epoch   
+    ref_time = refCoord_fast.obstime
 
-    gaiaCoord_shifted = gaiaCoord.apply_space_motion(new_obstime=ref_time)
-    sep = refCoord.separation(gaiaCoord_shifted)
-    pdb.set_trace()
-    ind = np.where(sep > 10. * u.deg)[0]
-    for i in ind:
-        sep[i] = 180.*u.deg - sep[i] # HACK
-    table[ang_dist_key] = sep.arcsec
-    table[ang_dist_key].unit = u.arcsec 
+    gaiaCoord_fast_shifted = gaiaCoord_fast.apply_space_motion(new_obstime=ref_time)
+    sep = refCoord_fast.separation(gaiaCoord_fast_shifted)
+    assert np.all(sep <= 10. * u.deg) # bug check
+
+    table[ang_dist_key][fast_mask] = sep.arcsec
+    table['pm_corrected'][fast_mask] = True
     table.sort([nasa_table_key, ang_dist_key]) # within each KIC/EPIC/host, sort by ang sep
 
     if not exoplanets:
         # save 20 arcsec radius:
         table = table[table[ang_dist_key] <= 20.]
-        table.write(outfile_20arcsec, format='fits', overwrite=True)
+        table.write(outfile_prefix+'20arcsec.fits', format='fits', overwrite=True)
         print('{0} stars with matches within 20 arcsec'.format(len(np.unique(table[nasa_table_key]))))
-        # cut down to 4 arcsec and save:
-        table = table[table[ang_dist_key] <= 4.]
-        table.write(outfile_4arcsec, format='fits', overwrite=True)
-        print('{0} stars with matches within 4 arcsec'.format(len(np.unique(table[nasa_table_key]))))
+    # cut down to 4 arcsec and save:
+    table = table[table[ang_dist_key] <= 4.]
+    table.write(outfile_prefix+'4arcsec.fits', format='fits', overwrite=True)
+    print('{0} stars with matches within 4 arcsec'.format(len(np.unique(table[nasa_table_key]))))
 
     # cut down to 1 arcsec and save:
     table = table[table[ang_dist_key] <= 1.]
-    table.write(outfile_1arcsec, format='fits', overwrite=True)
+    table.write(outfile_prefix+'1arcsec.fits', format='fits', overwrite=True)
     print('{0} stars with matches within 1 arcsec'.format(len(np.unique(table[nasa_table_key]))))  
     
 if __name__ == "__main__":
